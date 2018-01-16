c7ae9a7f577bb89817c294da8052762e776b4803
Jon Higgs <jhiggs@eml.cc>
2018-01-16 21:07:09 +1100

    Start testing

---
+++ lib/slug.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+fya_heading() {
+  if [[ -z "${MARKDOWN}" ]]; then
+    echo "MARKDOWN is undefined" >&2
+    return 1
+  fi
+
+  echo "${MARKDOWN}" \
+    | grep -m1 '^#' \
+    | sed -E 's/^[\#\ ]+//'
+}
+
+fya_slug() {
+  string=$1
+  echo "${string}" \
+    | tr [:upper:] [:lower:]  \
+    | sed 's/[^a-z0-9]/-/g'    \
+    | sed -E 's/-+/-/g'
+}
+
+fya_filename() {
+  string=$1
+  echo "$(date +%Y%m%d)-$(fya_slug ${string}).md"
+}
