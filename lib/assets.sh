afda1d453a68ee5f5711e40de50df74553194d46
Jon Higgs <jhiggs@eml.cc>
2018-01-18 07:31:55 +1100

    Make the asset_filename function

--- lib/assets.sh
+++ lib/assets.sh
@@ -1,3 +1,5 @@
+source $(dirname "${BASH_SOURCE[0]}")/slug.sh
+
 _image_refs() {
   if [[ -z "${MARKDOWN}" ]]; then
     echo "MARKDOWN is undefined" >&2
@@ -15,6 +17,14 @@ _asset_url() {
   ref=$1
   echo "${MARKDOWN}" \
     | awk '/^\['${ref}'\]:\ .*/ { print $2 }'
+}
+
+_asset_filename() {
+  ref=$1
+  slug=$(_slug "$(_heading)")
+  dir=$(_asset_directory "${slug}")
+  file="$(basename $(_asset_url "${ref}"))"
+  echo "${dir}/${file}"
 }
 
 # vim: ft=sh
