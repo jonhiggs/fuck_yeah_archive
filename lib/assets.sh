ba121e8658fc9b473ac6e6308b79a18812822e2c
Jon Higgs <jhiggs@eml.cc>
2018-01-20 07:32:56 +1100

    Move image refs to the page file

--- lib/assets.sh
+++ lib/assets.sh
@@ -1,17 +1,4 @@
-source $(dirname "${BASH_SOURCE[0]}")/slug.sh
-
-_image_refs() {
-  if [[ -z "${MARKDOWN}" ]]; then
-    echo "MARKDOWN is undefined" >&2
-    return 1
-  fi
-
-  echo "${MARKDOWN}" \
-    | grep -o -e '!\[.*\]\[.*\]' \
-    | sed -E 's/!\[.*\]\[([0-9]+)\]$/\1/g' \
-    | sort -n \
-    | uniq
-}
+source $(dirname "${BASH_SOURCE[0]}")/page.sh
 
 _asset_url() {
   ref=$1
@@ -21,7 +8,7 @@ _asset_url() {
 
 _asset_filename() {
   ref=$1
-  slug=$(_slug "$(_heading)")
+  slug=$(_slug "$(_page_heading)")
   dir=$(_asset_directory "${slug}")
   file="$(basename $(_asset_url "${ref}"))"
   echo "${dir}/${file}"
--- lib/page.sh
+++ lib/page.sh
@@ -11,4 +11,22 @@ _page_heading() {
     | sed -E 's/^[\#\ ]+//'
 }
 
+_page_refs() {
+  echo "not implemented" >&2
+}
+
+_page_image_refs() {
+  if [[ -z "${MARKDOWN}" ]]; then
+    echo "MARKDOWN is undefined" >&2
+    return 1
+  fi
+
+  echo "${MARKDOWN}" \
+    | grep -o -e '!\[.*\]\[.*\]' \
+    | sed -E 's/!\[.*\]\[([0-9]+)\]$/\1/g' \
+    | sort -n \
+    | uniq
+}
+
+
 # vim: ft=sh
