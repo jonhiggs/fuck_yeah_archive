d6b461519fec6cf7690bee83c3981c71169da973
Jon Higgs <jhiggs@eml.cc>
2018-01-28 08:58:00 +1100

    Make markdown contain local image reference

--- lib/fy/markdown.rb
+++ lib/fy/markdown.rb
@@ -7,6 +7,22 @@ module FuckYeahArchive
       @document = document
     end
 
+    def refs
+      response = {}
+      ref_types = @document.ref_types
+      @document.refs.each do |id,ref|
+        if ref_types[id] == ".html"
+          response[id] = ref
+        else
+          response[id] = File.join(
+            @document.asset_directory,
+            "#{id}#{ref_types[id]}"
+          )
+        end
+      end
+      response
+    end
+
     def render
       template = File.read(
         File.join(File.dirname(__FILE__), "../../template/markdown.erb")
@@ -15,7 +31,7 @@ module FuckYeahArchive
         :source => @document.source,
         :title => @document.title,
         :article => @document.article,
-        :refs => @document.refs,
+        :refs => refs,
       }
       ERB.new(template).result(OpenStruct.new(vars).instance_eval { binding })
     end
--- test/test_markdown.rb
+++ test/test_markdown.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 context "#FuckYeahArchive::Markdown" do
-  context "sample/1.md" do
+  context "sample/2.md" do
     setup do
-      FuckYeahArchive::Document.new(File.read("test/samples/1.md"))
+      input_doc = FuckYeahArchive::Document.new(File.read("test/samples/2.md"))
+      FuckYeahArchive::Markdown.new(input_doc)
+    end
+
+    context "refs" do
+      asserts("1") { topic.refs[1] }.equals("https://www.theawl.com/")
+      asserts("2") { topic.refs[2] }.equals("20180128-the-many-acts-of-keith-gordon/2.gif")
+      asserts("3") { topic.refs[3] }.equals("20180128-the-many-acts-of-keith-gordon/3.png")
+      asserts("4") { topic.refs[4] }.equals("http://www.theawl.com/")
+      asserts("5") { topic.refs[5] }.equals("20180128-the-many-acts-of-keith-gordon/5.jpg")
     end
   end
 end
